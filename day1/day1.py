7ea9ae630360faf1c18fc512a35856685768ee67
t-amandawong <amandamaenam@gmail.com>
2023-12-01 23:41:55 -0800

    cleaned up and commented day1 & day2 code

--- day1/day1.py
+++ day1/day1.py
@@ -1,48 +1,53 @@
 # aoc day 1 2023!!!
 
-calibration_sum = 0
 with open('puzzle_input.txt', 'r') as in_file:
     lines = [line.strip() for line in in_file]
 
+# part 1: each line contains a specific calibration value that is found by combining the first and last digit.
+# find each line's calibration value and submit the sum of all the calibration values
+calibration_sum = 0
 for line in lines:
-    calib = [i for i in line if i.isdigit()]
-    full_num = int(calib[0] + calib[-1])
-    # print(full_num)
-    calibration_sum += full_num
+    digit = [i for i in line if i.isdigit()]
+    calib = int(digit[0] + digit[-1])
+    calibration_sum += calib
 
-print(calibration_sum)
+print("Part 1:", calibration_sum)
 
-# part 2
-nums = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+# part 2: digits written out as "one", "two", "three", and so on, also count as digits
+# find each line's new calibration value (still first and last, but now including string words)
+# submit the sum of all the calibration values
+nums_dict = {'one': '1',
+             'two': '2',
+             'three': '3',
+             'four': '4',
+             'five': '5',
+             'six': '6',
+             'seven': '7',
+             'eight': '8',
+             'nine': '9'}
 
-nums_dict = {}
-for i in range(len(nums)):
-    nums_dict[nums[i]] = str(i + 1)
+true_sum = 0
+for line in lines:
+    # valid_digits stores the index where each valid digit is along with the digit itself in 'index: digit' pair
+    valid_digits = {}
+    # check for string nums and add its index and digit version into valid_digits dictionary
+    for num in nums_dict.keys():
+        if line.find(num) != -1:
+            all_matches = [i for i in range(len(line)) if line.startswith(num, i)]
+            for index in all_matches:
+                valid_digits[index] = nums_dict.get(num)
+    # check for digit nums and its index and digit itself into valid_digits dictionary
+    for i in range(len(line)):
+        if line[i].isdigit():
+            valid_digits[i] = line[i]
 
-#print(nums_dict)
+    # sort valid_digits by index & find first and last
+    sorted_digits = dict(sorted(valid_digits.items()))
+    first_index = list(sorted_digits.keys())[0]
+    last_index = list(sorted_digits.keys())[-1]
 
-all_calibs = 0
-with open('puzzle_input.txt', 'r') as in_file:
-    for line in in_file:
-        #print(line.strip() + ":", end=' ')
-        valid_calibs = {}
-        if line.strip():
-            for num in nums:
-                if line.find(num) != -1:
-                    all_matches = [i for i in range(len(line.strip())) if line.strip().startswith(num, i)]
-                    for index in all_matches:
-                        valid_calibs[index] = nums_dict.get(num)
-            for i in range(len(line)):
-                if line[i].isdigit():
-                    valid_calibs[i] = line[i]
-            sorted_calibs = dict(sorted(valid_calibs.items()))
-            #print(sorted_calibs)
+    # find full_calib by concatenating first and last digits
+    full_calib = sorted_digits[first_index] + sorted_digits[last_index]
+    true_sum += int(full_calib)
 
-            first_index = list(sorted_calibs.keys())[0]
-            last_index = list(sorted_calibs.keys())[-1]
-
-            full_calib = sorted_calibs[first_index] + sorted_calibs[last_index]
-            #print(int(full_calib))
-            all_calibs += int(full_calib)
-
-print(all_calibs)
+print("Part 2:", true_sum)
--- day2/day2.py
+++ day2/day2.py
@@ -1,41 +1,50 @@
-max_red = 12
-max_green = 13
-max_blue = 14
+# aoc day 2 2023
 
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+# each line is a game in which colored cubes (red, green, or blue) are shown in multiple sets
+# add a list of each game's sets to the larger games list
 games = list()
 with open('input.txt') as in_file:
     for line in in_file:
         if line.strip:
-            stripped_line = line.strip()
-            game = stripped_line.split(": ")[1].split("; ")
+            game = line.strip().split(": ")[1].split("; ")
             games.append(game)
 
+# in part 1, add up the IDs for each game that is possible if the bag only contains 12 red, 13 green, and 14 blue
+# the IDs(each game's index in games list + 1) will be stored in valid_games list, and its sum will be submitted
+
+# in part 2, for each game, find the minimum amount of cubes needed for each color to make the game possible
+# the powers for each game(product of min red, min green, and min blue cubes) will be stored in powers list
+# the sum of the powers list is submitted for part 2
 valid_games = list()
-min_powers = list()
+powers = list()
 for i in range(len(games)):
+    # initialize the current max for each color to 0 for every game
     curr_red = 0
-    curr_blue = 0
     curr_green = 0
-    for subgame in games[i]:
-        colors = subgame.split(", ")
-        for color in colors:
-            num_color = color.split()
-            num = int(num_color[0])
-            if num_color[1] == 'red':
-                if num > curr_red:
-                    curr_red = num
-            if num_color[1] == 'blue':
-                if num > curr_blue:
-                    curr_blue = num
-            if num_color[1] == 'green':
-                if num > curr_green:
-                    curr_green = num
-    # for part 1
-    if curr_red <= max_red and curr_blue <= max_blue and curr_green <= max_green:
+    curr_blue = 0
+    # for each set, split the string to just the number and color combinations
+    for cube_sets in games[i]:
+        num_colors = cube_sets.split(", ")
+        for num_colors in num_colors:
+            num, color = num_colors.split()
+            num = int(num)
+            # check which color the number corresponds to and update curr_color accordingly
+            if color == 'red':
+                curr_red = max(curr_red, num)
+            if color == 'green':
+                curr_green = max(curr_green, num)
+            if color == 'blue':
+                curr_blue = max(curr_blue, num)
+    # (part 1) check that the current value for each color is less than its max, and add to valid_games if so
+    if curr_red <= MAX_RED and curr_green <= MAX_GREEN and curr_blue <= MAX_BLUE:
         valid_games.append(i+1)
-    # for part 2
-    min_powers.append(curr_red * curr_blue * curr_green)
+    # (part 2) the minimum amount needed to play each game is actually the max of each color, so
+    # multiply each current_color together to obtain the power for this game
+    powers.append(curr_red * curr_blue * curr_green)
 
-
-print(sum(valid_games))
-print(sum(min_powers))
+print("Part 1:", sum(valid_games))
+print("Part 2:", sum(powers))
